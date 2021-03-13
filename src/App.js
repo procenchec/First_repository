f3b2a736a1857a702f8cd09ad75929ee2084eff2
Bogdan Artemyev <bogarto@mail.ru>
2021-03-13 12:15:44 +0300

    Artemiev

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Protsenko from './containers/Protsenko'
+import Artemyev from './containers/Artemyev'
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
           Learn React
         </a>
       </header>*/}
-      <Protsenko>
+      <Artemyev>
         
-      </Protsenko>
+      </Artemyev>
     </div>
   );
 }
