030296845c87aadbd48364c8adde7e32b34e2e31
procenchec <procenchec@gmail.com>
2021-03-13 11:22:28 +0300

    'first commit'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Protsenko from './containers/Protsenko'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
+      <Protsenko>
+        
+      </Protsenko>
     </div>
   );
 }
---
+++ src/containers/Protsenko.js
@@ -0,0 +1,14 @@
+// import logo from '../logo.svg';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="Procenko">
+        <img src={'https://i.gifer.com/7hY5.gif'} className="App-logo" alt="logo" />
+            
+        Проценко ТО
+    </div>
+  );
+}
+
+export default App;
