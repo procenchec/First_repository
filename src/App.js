dcf1d82e7ffd9062876a4f963d0434a4d7d77000
Rozhkova <rozhka1999@gmail.com>
2021-03-13 12:38:31 +0300

    Рожкова

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Protsenko from './containers/Protsenko'
+import Rozhkova from './containers/Rozhkova'
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
           Learn React
         </a>
       </header>*/}
-      <Protsenko>
+      <Rozhkova>
         
-      </Protsenko>
+      </Rozhkova>
     </div>
   );
 }
