a62c0d472da460d7d8156ab77c2885240c063721
olimp <olimp@university.kspu.kaluga.ru>
2021-03-25 18:29:18 +0300

    Лб5

--- src/containers/Kamleva.js
+++ src/containers/Kamleva.js
@@ -1,6 +1,8 @@
 // import logo from '../logo.svg';
 
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
 
 function Example() {
   const [count, setCount] = useState(0);
@@ -21,14 +23,26 @@ function Example() {
 
 function App() {
   return (
-    <div className="Kamleva">
+    <Kaml>
         <img src={'https://99px.ru/sstorage/86/2015/07/image_861007151003132215733.gif'} className="App-logo" alt="logo" />
             
         Камлёва Н.С. Б-ИСиТ-41
 
      <Example />
-    </div>
+    </Kaml>
   );
 }
+
+const Kaml = styled.div`
+  background-color: rgb(226, 243, 74);
+  text-align: center;
+  color: rgb(221, 44, 44);
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
 
 export default App;
