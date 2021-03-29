dcb1749be8ce8824ecdbd286e2ca60d9c981a2ab
rozhkova <rozhka1999@gmail.com>
2021-03-29 19:17:31 +0300

    Lab 6

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,8 +1,10 @@
+import sheglovaLabs from './sheglova';
+
 let i = 0;
 
-export const gen = (name, email) => {
+export const gen = (name, email, labs) => {
     i++;
-    return { name, email, id: i }
+    return { name, email, id: i, ...labs }
 }
-const list = [gen('Камлёва', ''), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', ''), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '')];
+const list = [gen('Камлёва', ''), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', ''), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '', sheglovaLabs)];
 export default list
