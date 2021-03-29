a3b80e1383cc3a7560ef52187bd7cd1796d446a1
procenchec <procenchec@gmail.com>
2021-03-29 19:14:14 +0300

    Lab 6

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,8 +1,10 @@
+import kamlevaLabs from './kamleva';
+
 let i = 0;
 
-export const gen = (name, email) => {
+export const gen = (name, email, labs) => {
     i++;
-    return { name, email, id: i }
+    return { name, email, id: i, ...labs }
 }
-const list = [gen('Камлёва', ''), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', ''), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '')];
+const list = [gen('Камлёва', 'KamljovaNS@studklg.ru', kamlevaLabs), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', ''), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '')];
 export default list
