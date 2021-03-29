9903ebc174b2b7f2e6cd07b1190be87ed41ad1a0
mikalyuk <mickalyuck123@mail.ru>
2021-03-29 19:23:18 +0300

    LR5 Artemyev

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,8 +1,13 @@
+import labsArtemev from './artemyev';
+
 let i = 0;
 
-export const gen = (name, email) => {
+export const gen = (name, email, labsArtemev) => {
     i++;
-    return { name, email, id: i }
+    return { name, email, ...labsArtemev, id: i }
 }
-const list = [gen('Камлёва', ''), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', ''), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '')];
+
+const list = [gen('Камлёва', ''), gen('Миронов', ''), gen('Косырев', ''), gen('Артемьев', 'bogarto@mail.ru', labsArtemev), gen('Проценко', ''), gen('Рожкова', ''), gen('Тараканов', ''), gen('Белявский', ''), gen('Корнеев', ''), gen('Лобанов', ''), gen('Щеглова', '')];
+
+
 export default list
