f941bf45919dd277486e3afebca01c23676d77be
expert40rus <vladik.monstor@mail.ru>
2021-04-08 13:32:13 +0300

    Добавил отчеты

--- src/constants/nikulin.js
+++ src/constants/nikulin.js
@@ -5,6 +5,8 @@ import lr3 from "../assets/NikulinN/3.pdf";
 import lr4 from "../assets/NikulinN/4.pdf";
 import lr5 from "../assets/NikulinN/5.pdf";
 import lr6 from "../assets/NikulinN/6.pdf";
+import lr7 from "../assets/NikulinN/7.pdf";
+import lr8 from "../assets/NikulinN/8.pdf";
 import {cloneElement} from "react";
 import React from "react";
 
@@ -15,4 +17,6 @@ export default {
     l4: cloneElement(<Iframe></Iframe>, {src: lr4}),
     l5: cloneElement(<Iframe></Iframe>, {src: lr5}),
     l6: cloneElement(<Iframe></Iframe>, {src: lr6}),
+    l7: cloneElement(<Iframe></Iframe>, {src: lr7}),
+    l8: cloneElement(<Iframe></Iframe>, {src: lr8}),
 }
