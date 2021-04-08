b8f11f9fefac38dc844c6957575dc1f7ac0752f0
Kimon1112 <BelyavskijVV@studklg.ru>
2021-04-08 13:28:57 +0300

    1

--- src/constants/nikulin.js
+++ src/constants/nikulin.js
@@ -1,10 +1,10 @@
 import Iframe from "../components/Iframe";
-import lr1 from "../assets/Tarakanov/lr1.pdf";
-import lr2 from "../assets/Tarakanov/lr2.pdf";
-import lr3 from "../assets/Tarakanov/lr3.pdf";
-import lr4 from "../assets/Tarakanov/lr4.pdf";
-import lr5 from "../assets/Tarakanov/lr5.pdf";
-import lr6 from "../assets/Tarakanov/lr6.pdf";
+import lr1 from "../assets/NikulinN/1.pdf";
+import lr2 from "../assets/NikulinN/2.pdf";
+import lr3 from "../assets/NikulinN/3.pdf";
+import lr4 from "../assets/NikulinN/4.pdf";
+import lr5 from "../assets/NikulinN/5.pdf";
+import lr6 from "../assets/NikulinN/6.pdf";
 import {cloneElement} from "react";
 import React from "react";
 
@@ -13,6 +13,6 @@ export default {
     l2: cloneElement(<Iframe></Iframe>, {src: lr2}),
     l3: cloneElement(<Iframe></Iframe>, {src: lr3}),
     l4: cloneElement(<Iframe></Iframe>, {src: lr4}),
-    l5: "bruh",
-    l6: "bruh"
+    l5: cloneElement(<Iframe></Iframe>, {src: lr5}),
+    l6: cloneElement(<Iframe></Iframe>, {src: lr6}),
 }
