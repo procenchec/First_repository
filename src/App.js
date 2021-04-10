5d2764aa065b947ffef7a13efa05db8645929bdf
KorneevAM <ya.sassa2013@yandex.rum>
2021-04-10 10:34:55 +0300

    lr8

--- src/App.js
+++ src/App.js
@@ -1,74 +1,86 @@
-import './App.css';
-import Sheglova from './containers/Sheglova'
-import Procenko from './containers/Protsenko'
-import Artemyev from './containers/Artemyev'
-import Belyavskii from './containers/Belyavskii'
-import Lobanov from './containers/Lobanov'
-import Rozhkova from './containers/Rozhkova'
-import Kamleva from './containers/Kamleva'
-import Turov from './containers/Turov'
-import Mironov from './containers/Mironov'
+import "./App.css";
+import Sheglova from "./containers/Sheglova";
+import Procenko from "./containers/Protsenko";
+import Artemyev from "./containers/Artemyev";
+import Belyavskii from "./containers/Belyavskii";
+import Lobanov from "./containers/Lobanov";
+import Rozhkova from "./containers/Rozhkova";
+import Kamleva from "./containers/Kamleva";
+import Turov from "./containers/Turov";
+import Mironov from "./containers/Mironov";
 import Nikulin from "./containers/Nikulin";
 import Korneev from "./containers/Korneev";
 import Godonoga from "./containers/Godonoga";
 import Mikalyuk from "./containers/Mikalyuk";
-import List from './containers/Common/List';
-import UrlKamleva from './containers/Kamleva/Url';
+import List from "./containers/Common/List";
+import UrlKamleva from "./containers/Kamleva/Url";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Lb7 from './containers/Common/LB7';
+import Lb7 from "./containers/Common/LB7";
+import styled from "styled-components";
 
+const Input = styled(Lb7)`
+  
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+  font-weight: 900;
+`;
+
+const Header = styled.div`
+    background-color: rgb(21, 150, 205);
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+  font-weight: 900;
+  color: rgb(20, 19, 89);
+  min-height: 6vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <Lb7 />
-                <UrlKamleva />
-                <Switch>
-                    <Route path="/Камлёва">
-                        <Kamleva />
-                    </Route>
-                    <Route path="/Проценко">
-                        <Procenko />
-                    </Route>
-                    <Route path="/Артемьев">
-                        <Artemyev />
-                    </Route>
-                    <Route path="/">
-                        <a href="https://protsenkoweb.herokuapp.com/">https://protsenkoweb.herokuapp.com/</a>
-                        <List />
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <UrlKamleva />
+        <Switch>
+          <Route path="/Камлёва">
+            <Kamleva />
+          </Route>
+          <Route path="/Проценко">
+            <Procenko />
+          </Route>
+          <Route path="/Артемьев">
+            <Artemyev />
+          </Route>
+          <Route path="/">
+            <Header>
+              <Input />
+              <a href="https://protsenkoweb.herokuapp.com/">
+                https://protsenkoweb.herokuapp.com/
+              </a>
+            </Header>
 
-                        <Lobanov>
+            <List />
 
-                        </Lobanov>
-                        <Rozhkova>
-                        </Rozhkova>
+            <Lobanov></Lobanov>
+            <Rozhkova></Rozhkova>
 
-                        <Turov>
+            <Turov></Turov>
+            <Korneev></Korneev>
+            <Mironov></Mironov>
+            <Mikalyuk></Mikalyuk>
 
-                        </Turov>
-                        <Korneev>
-                        </Korneev>
-                        <Mironov>
+            <Belyavskii></Belyavskii>
 
-                        </Mironov>
-                        <Mikalyuk>
-
-                        </Mikalyuk>
-
-                        <Belyavskii>
-                        </Belyavskii>
-
-                        <Nikulin>
-
-                        </Nikulin>
-                        <Godonoga>
-                        </Godonoga>
-                    </Route>
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+            <Nikulin></Nikulin>
+            <Godonoga></Godonoga>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
