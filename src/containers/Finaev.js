88a192f7554d7713057b74b896a9d267f1963365
rozhkova <rozhka1999@gmail.com>
2021-04-19 15:51:51 +0300

    UseEffect

--- src/containers/Finaev.js
+++ src/containers/Finaev.js
@@ -1,16 +1,38 @@
-// import logo from '../logo.svg';
-import '../App.css'
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+function Example() {
+  const state = useState(0);
+  const [count, setCount] = state;
 
+  useEffect(() => {
+    if (count === 10) {
+      alert('10');
+    }
+  }, [count]);
 
+  return (
+    <div>
 
+      {count === 5 && <div>Ура, пять</div>}
+
+      <p> {state[0]} </p>
+      <button class="button" onClick={() => state[1](count - 1)}>
+        -1
+      </button> &nbsp;
+
+      <button class="button" onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+    </div>
+  );
+}
 
 const AppContainer = styled.div`
   font-family: sans-serif;
   text-align: center;
-  background-color: rgb(77, 93, 182);
+  background-color: rgb(205, 74, 76);
   text-align: center;
   color: rgb(0, 0, 0);
   min-height: 100vh;
@@ -27,13 +49,11 @@ const Image = styled.img`
 `
 const App = () => (
   <AppContainer>
-     <Image src={'https://i.imgur.com/Kxghvcb.gif'}/>
-
+    <Image src={'https://i.imgur.com/Kxghvcb.gif'} />
      Финаев А.С. Б-ИСиТ 41
+    <Example />
   </AppContainer>
 )
-
-
 
 export default App;
 
